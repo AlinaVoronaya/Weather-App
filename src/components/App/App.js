e0c7efe22d1942cbacf40a19a8c0225a986e6b80
Alina Voronaya <Alinavoronaya@mail.ru>
2022-08-03 17:32:41 +0500

    Добавила погоду по геолокации

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,42 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import {Info} from "../Info/Info";
 import {Form} from "../Form/Form";
 import './App.css';
 
+const mapWeatherConditions = (string) => {
+    if (string === "Clouds") {
+        return "Облачно"
+    }
+    return "";
+}
 
 function App() {
 
     const [data, setData] = useState({});
     const [location, setLocation] = useState('');
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=c64a374174e692bfd1fc25bb4d1ed61c&lang=ru&units=metric`
+    const urlCity = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=c64a374174e692bfd1fc25bb4d1ed61c&lang=ru&units=metric`
+
+    useEffect(() => {
+        console.log("UseEffect");
+        navigator.geolocation.getCurrentPosition((position) => {
+            getWeather(position.coords.latitude,
+                position.coords.longitude)
+        }, (err) => console.log(err.message));
+    },[])
+
+    const getWeather = (latitude, longitude) => {
+        const urlLocation = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=c64a374174e692bfd1fc25bb4d1ed61c&lang=ru&units=metric`
+        axios.get(urlLocation).then((response) => {
+            setData(response.data)
+            console.log(response)
+        })
+    }
 
     const searchLocation = (event) => {
         if (event.key === 'Enter') {
-            axios.get(url).then((response) => {
+            axios.get(urlCity).then((response) => {
                 setData(response.data)
                 console.log(response.data)
             })
@@ -45,22 +67,22 @@ function App() {
                         {data.main ? <h1>{data.main.temp.toFixed()}°</h1> : null}
                     </div>
                     <div className="description">
-                        {data.weather ? <p>{data.weather[0].main}</p> : null}
+                        {data.weather ? <p>{mapWeatherConditions(data.weather[0].main)}</p> : null}
                     </div>
                 </div>
 
                 {data.name !== undefined &&
                     <div className="bottom">
                         <div className="feels">
-                            {data.main ? <p className="bold">{data.main.feels_like.toFixed()}°</p> : null}
+                            {data.main ? <p>{data.main.feels_like.toFixed()}°</p> : null}
                             <p>Ощущается как</p>
                         </div>
                         <div className="humidity">
-                            {data.main ? <p className="bold">{data.main.humidity} %</p> : null}
+                            {data.main ? <p>{data.main.humidity} %</p> : null}
                             <p>Влажность</p>
                         </div>
                         <div className="wind">
-                            {data.wind ? <p className="bold">{data.wind.speed} м/с</p> : null}
+                            {data.wind ? <p>{data.wind.speed.toFixed()} м/с</p> : null}
                             <p>Скорость ветра</p>
                         </div>
                     </div>
